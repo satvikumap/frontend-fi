683d0cbb0c1d1c0224123322a571a97766ce9fef
satvik <Satvik@Mac.lan>
2025-01-11 22:45:14 +0530

    add

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1', // Update with your backend URL
+    baseURL: 'https://backend-fri.onrender.com/api/v1', // Update with your backend URL
     withCredentials: true, // If you are handling cookies or sessions
 });
 
